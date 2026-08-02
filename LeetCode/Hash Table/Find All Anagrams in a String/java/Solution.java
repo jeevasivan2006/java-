7d04b3771b3d3daad124ad7c4403ea5a2a7e9b87
Jeeva S <jeevajeeva1672@gmail.com>
2026-08-02 15:05:19 +0530

    feat: Add Find All Anagrams in a String

--- LeetCode/Hash Table/Find All Anagrams in a String/java/Solution.java
+++ LeetCode/Hash Table/Find All Anagrams in a String/java/Solution.java
@@ -1,13 +1,13 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        char s3[]=p.toCharArray();
-        Arrays.sort(s3);
+        char ch[]=p.toCharArray();
+        Arrays.sort(ch);
         List<Integer> list=new ArrayList<>();
         for(int i=0;i<=s.length()-p.length();i++){
-            String c=s.substring(i,i+p.length());
-            char ch[]=c.toCharArray();
-            Arrays.sort(ch);
-            if(Arrays.equals(ch,s3)){
+            String ss=s.substring(i,i+p.length());
+            char c[]=ss.toCharArray();
+            Arrays.sort(c);
+            if(Arrays.equals(c,ch)){
                 list.add(i);
             }
         }
