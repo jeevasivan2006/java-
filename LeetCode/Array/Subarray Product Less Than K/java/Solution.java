cb9cd0a8f1e65f156b1213ab6f716f2483c7b12c
Jeeva S <jeevajeeva1672@gmail.com>
2026-08-03 11:10:42 +0530

    feat: Add Subarray Product Less Than K

--- LeetCode/Array/Subarray Product Less Than K/java/Solution.java
+++ LeetCode/Array/Subarray Product Less Than K/java/Solution.java
@@ -1,16 +1,34 @@
 class Solution {
-    public int numSubarrayProductLessThanK(int[] nums, int k) {
-        if (k <= 1) return 0;
-        int totalCount = 0;
-        int product = 1;
-        for (int left = 0, right = 0; right < nums.length; right++) {
-            product *= nums[right];
-            while (product >= k) {
-                product /= nums[left++];
+    public int numSubarrayProductLessThanK(int[] nums, int kk) {
+        // if (kk <= 1) return 0;
+        // int totalCount = 0;
+        // int j=0;
+        // while(j<nums.length){
+        //     for(int i=0;i<nums.length-j;i++){
+        //         int arr[]=Arrays.copyOfRange(nums,i,i+j);
+        //          int product = 1;
+        //     for(int k=0;k<arr.length;k++){
+        //         product*=arr[k];
+        //     }
+        //     if(product<kk){
+        //         totalCount++;
+        //     }
+        //     }
+        //    j++;
+        // }
+        // return totalCount;
+        int t=0;
+        for(int i=0;i<nums.length;i++){
+            int p=1;
+            for(int j=i;j<nums.length;j++){
+                p*=nums[j];
+                if(p<kk){
+                    t++;
+                }else{
+                    break;
+                }
             }
-            totalCount += right - left + 1;  
         }
-
-        return totalCount;
+        return t;
     }
 }
