5ab9d2cbfefcee28894325eecc6a678b82b59ddc
Jeeva S <jeevajeeva1672@gmail.com>
2026-08-03 11:13:32 +0530

    feat: Add Subarray Product Less Than K

--- LeetCode/Array/Subarray Product Less Than K/java/Solution.java
+++ LeetCode/Array/Subarray Product Less Than K/java/Solution.java
@@ -1,34 +1,37 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int kk) {
-        // if (kk <= 1) return 0;
-        // int totalCount = 0;
-        // int j=0;
-        // while(j<nums.length){
-        //     for(int i=0;i<nums.length-j;i++){
-        //         int arr[]=Arrays.copyOfRange(nums,i,i+j);
-        //          int product = 1;
-        //     for(int k=0;k<arr.length;k++){
-        //         product*=arr[k];
-        //     }
-        //     if(product<kk){
-        //         totalCount++;
-        //     }
-        //     }
-        //    j++;
-        // }
-        // return totalCount;
-        int t=0;
-        for(int i=0;i<nums.length;i++){
-            int p=1;
-            for(int j=i;j<nums.length;j++){
-                p*=nums[j];
-                if(p<kk){
-                    t++;
-                }else{
-                    break;
-                }
+        if (kk <= 1) return 0;
+        int totalCount = 0;
+        int j=0;
+       // while(j<nums.length){
+            for(int i=0;i<nums.length-j;i++){
+               // int arr[]=Arrays.copyOfRange(nums,i,i+j);
+                 int product = 1;
+            for(int k=i;k<nums.length;k++){
+                product*=nums[k];
+            
+            if(product<kk){
+                totalCount++;
             }
-        }
-        return t;
+            else {
+                break;
+            }}
+            }
+        //   j++;
+        //}
+        return totalCount;
+        // int t=0;
+        // for(int i=0;i<nums.length;i++){
+        //     int p=1;
+        //     for(int j=i;j<nums.length;j++){
+        //         p*=nums[j];
+        //         if(p<kk){
+        //             t++;
+        //         }else{
+        //             break;
+        //         }
+        //     }
+        // }
+        // return t;
     }
 }
